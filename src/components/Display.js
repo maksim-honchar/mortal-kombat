402831fd5b509d5cb6def354b720aef8a0be38a2
Maksym Honchar <maksim.honchar@gmail.com>
2020-08-27 18:29:39 +0300

    remove nanoid

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,32 +1,31 @@
 import React from "react";
 import "./Display.css";
-import { nanoid } from "nanoid";
 import images from "../images";
 
 function Display() {
-  const [mark, setMark] = React.useState(0);
+    const [mark, setMark] = React.useState(0);
 
-  const imageTile = images.map((item, index) => (
-    <div className="div-img" key={nanoid()}>
-      <img
-        className={index === mark ? "pics-active" : "pics-passive"}
-        src={item.src}
-        key={nanoid()}
-        alt={item.id}
-      />
-    </div>
-  ));
+    const imageTile = images.map((item, index) => (
+        <div className="div-img" key={item.id}>
+            <img
+                className={index === mark ? "pics-active" : "pics-passive"}
+                src={item.src}
+                key={item.id}
+                alt={item.id}
+            />
+        </div>
+    ));
 
-  window.addEventListener("keydown", (e) => {
-    if (e.code === "ArrowRight") {
-      setMark(mark + 1);
-    } else if (e.code === "ArrowLeft") {
-      setMark(mark - 1);
-    }
-    console.log(mark);
-  });
+    window.addEventListener("keydown", (e) => {
+        if (e.code === "ArrowRight") {
+            setMark(mark + 1);
+        } else if (e.code === "ArrowLeft") {
+            setMark(mark - 1);
+        }
+        console.log(mark);
+    });
 
-  return <div className="display-wrapper">{imageTile}</div>;
+    return <div className="display-wrapper">{imageTile}</div>;
 }
 
 export default Display;
