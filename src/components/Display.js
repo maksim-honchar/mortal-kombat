8a500b708739dc097cdc210d977691e670fb9c6e
Maksym Honchar <maksim.honchar@gmail.com>
2020-08-27 16:21:19 +0300

    add move rigth and left

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import './Display.css'
-import { nanoid } from 'nanoid'
-import images from '../images'
-
-
+import React from "react";
+import "./Display.css";
+import { nanoid } from "nanoid";
+import images from "../images";
 
 function Display() {
-    const [mark, setMark] = React.useState('Goro')
+  const [mark, setMark] = React.useState(0);
 
-    const imageTile = images.map(item =>
-        <div
-            className='div-img'
-            key={nanoid()}
-        >
-            <img
-                className={item.id === mark ? 'pics-active' : 'pics-passive'}
-                src={item.src}
-                key={nanoid()}
-                alt={item.id}
-            />
-        </div>
-    )
+  const imageTile = images.map((item, index) => (
+    <div className="div-img" key={nanoid()}>
+      <img
+        className={index === mark ? "pics-active" : "pics-passive"}
+        src={item.src}
+        key={nanoid()}
+        alt={item.id}
+      />
+    </div>
+  ));
 
-    window.addEventListener('keydown', (e) => {
-        if (e.code === 'ArrowRight') {
-            console.log('ArrowRight')
-        } else if (e.code === 'ArrowLeft') {
-            console.log('ArrowLeft')
-        }
-    })
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "ArrowRight") {
+      setMark(mark + 1);
+    } else if (e.code === "ArrowLeft") {
+      setMark(mark - 1);
+    }
+    console.log(mark);
+  });
 
-    return (
-        <div className='display-wrapper'>
-            {imageTile}
-        </div>
-    )
+  return <div className="display-wrapper">{imageTile}</div>;
 }
 
-export default Display
+export default Display;
