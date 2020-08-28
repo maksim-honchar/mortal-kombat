49018c836509fceab542a7194b8bfa5a810556f9
Maksym Honchar <maksim.honchar@gmail.com>
2020-08-28 19:59:39 +0300

    format document

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,30 +1,33 @@
-import React from "react";
-import "./Display.css";
-import images from "../images";
+import React from 'react'
+import './Display.css'
+import images from '../images'
 
 function Display() {
-    const [mark, setMark] = React.useState(0);
+    const [mark, setMark] = React.useState(0)
 
     const imageTile = images.map((item, index) => (
-        <div className="div-img" key={item.id}>
+        <div className='div-img' key={item.id}>
             <img
-                className={index === mark ? "pics-active" : "pics-passive"}
+                className={index === mark ? 'pics-active' : 'pics-passive'}
                 src={item.src}
                 key={item.id}
                 alt={item.id}
             />
         </div>
-    ));
+    ))
 
-    window.addEventListener("keydown", (e) => {
-        if (e.code === "ArrowRight") {
-            setMark(mark + 1);
-        } else if (e.code === "ArrowLeft") {
-            setMark(mark - 1);
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'ArrowRight') {
+            setMark(mark + 1)
+        } else if (e.code === 'ArrowLeft') {
+            setMark(mark - 1)
         }
-    });
+        console.log(mark)
+    })
 
-    return <div className="display-wrapper">{imageTile}</div>;
+    return (
+        <div className='display-wrapper'>{imageTile}</div>
+    )
 }
 
-export default Display;
+export default Display
