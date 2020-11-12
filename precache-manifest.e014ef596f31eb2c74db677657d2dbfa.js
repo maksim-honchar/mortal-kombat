72de2516d7ea9d7fe61702ef42b31ef789766d37
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-12 13:45:27 +0200

    Updates

---
+++ precache-manifest.e014ef596f31eb2c74db677657d2dbfa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222f0c3ee0df1861fd8c7cd8e328e9de",
+    "url": "/mortal-kombat/index.html"
+  },
+  {
+    "revision": "1cf28f833627b73bd306",
+    "url": "/mortal-kombat/static/css/main.ead3ca40.chunk.css"
+  },
+  {
+    "revision": "411c90bac362afa8453b",
+    "url": "/mortal-kombat/static/js/2.1e48afff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mortal-kombat/static/js/2.1e48afff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf28f833627b73bd306",
+    "url": "/mortal-kombat/static/js/main.31578e6d.chunk.js"
+  },
+  {
+    "revision": "0b24d7ef4154e4615c6e",
+    "url": "/mortal-kombat/static/js/runtime-main.8c49247d.js"
+  },
+  {
+    "revision": "46266e9265493225770ac48bd70d1e0e",
+    "url": "/mortal-kombat/static/media/Baraka.46266e92.png"
+  },
+  {
+    "revision": "a94efb5285266b30c149de198f39460a",
+    "url": "/mortal-kombat/static/media/CassieCage.a94efb52.png"
+  },
+  {
+    "revision": "9f3715bed4abd8e56b3b667c7e004428",
+    "url": "/mortal-kombat/static/media/ErronBlack.9f3715be.png"
+  },
+  {
+    "revision": "119a390323b8eee18e2a07177f527f0e",
+    "url": "/mortal-kombat/static/media/Goro.119a3903.png"
+  },
+  {
+    "revision": "47cfa472fbaaf1a307eb6ca7a9a4a0a1",
+    "url": "/mortal-kombat/static/media/Jax.47cfa472.png"
+  },
+  {
+    "revision": "80f2a353fb4efe50db4a33758494a760",
+    "url": "/mortal-kombat/static/media/JohnnyCage.80f2a353.png"
+  },
+  {
+    "revision": "4c71895d84ed869fec187be105144e64",
+    "url": "/mortal-kombat/static/media/Kabal.4c71895d.png"
+  },
+  {
+    "revision": "e13afd980d317e69803abbe5866a2553",
+    "url": "/mortal-kombat/static/media/Kano.e13afd98.png"
+  },
+  {
+    "revision": "b3b1d4bf24d60f8afe207539445e6048",
+    "url": "/mortal-kombat/static/media/Kitana.b3b1d4bf.png"
+  },
+  {
+    "revision": "7c33571e5f06009075d68dcdb181dc23",
+    "url": "/mortal-kombat/static/media/KotalKahn.7c33571e.png"
+  },
+  {
+    "revision": "5e9e4f9eef116ae745118db30d741d59",
+    "url": "/mortal-kombat/static/media/KungLao.5e9e4f9e.png"
+  },
+  {
+    "revision": "2e4ef69211a10ecc75d59d266593d4fe",
+    "url": "/mortal-kombat/static/media/LiuKang.2e4ef692.png"
+  },
+  {
+    "revision": "b58b49ea289f0a8f9b58774f68dbe487",
+    "url": "/mortal-kombat/static/media/NoobSaibot.b58b49ea.png"
+  },
+  {
+    "revision": "2483d7f1cf5393fa4cddd22416803c82",
+    "url": "/mortal-kombat/static/media/Raiden.2483d7f1.png"
+  },
+  {
+    "revision": "c3f4ccf49fa2f0e10854d0f602dac81b",
+    "url": "/mortal-kombat/static/media/Reptile.c3f4ccf4.png"
+  },
+  {
+    "revision": "b84a00a89c788b23400f264197234a73",
+    "url": "/mortal-kombat/static/media/Scorpion.b84a00a8.png"
+  },
+  {
+    "revision": "a93d82048f96d1287d8e1f8d774e1986",
+    "url": "/mortal-kombat/static/media/ShangTsung.a93d8204.png"
+  },
+  {
+    "revision": "25ff8e1ab26fc2cca1cef6fb8939d341",
+    "url": "/mortal-kombat/static/media/ShaoKahn.25ff8e1a.png"
+  },
+  {
+    "revision": "fc2d0cc0b49a9aabc05d1e627a374f38",
+    "url": "/mortal-kombat/static/media/SonyaBlade.fc2d0cc0.png"
+  },
+  {
+    "revision": "f4f73065586e257f48f8177a509a6e81",
+    "url": "/mortal-kombat/static/media/SubZero.f4f73065.png"
+  }
+]);
